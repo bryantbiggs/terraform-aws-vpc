e203d5fe1e886c9521ba82aa9c28da35d9f15b35
Bryant Biggs <bryantbiggs@gmail.com>
2025-09-26 12:12:48 -0500

    chore: Combine IPv6 examples into one

--- examples/ipv6/main.tf
+++ examples/ipv6/main.tf
@@ -8,6 +8,9 @@ locals {
   name   = "ex-${basename(path.cwd)}"
   region = "eu-west-1"
 
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
   tags = {
     Example    = local.name
     GithubRepo = "terraform-aws-vpc"
@@ -19,12 +22,13 @@ locals {
 # VPC Module
 ################################################################################
 
-module "vpc" {
+module "vpc_ipv6_only" {
   source = "../.."
 
   name = local.name
+  cidr = local.vpc_cidr
+  azs  = local.azs
 
-  azs         = slice(data.aws_availability_zones.available.names, 0, 3)
   enable_ipv6 = true
 
   public_subnet_ipv6_native    = true
@@ -38,6 +42,33 @@ module "vpc" {
 
   enable_nat_gateway     = false
   create_egress_only_igw = true
+
+  tags = local.tags
+}
+
+
+module "vpc_dualstack" {
+  source = "../.."
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs              = local.azs
+  private_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  public_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
+  database_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 8)]
+
+  enable_nat_gateway = true
+
+  create_database_subnet_route_table     = true
+  create_database_internet_gateway_route = true
+
+  enable_ipv6                                   = true
+  public_subnet_assign_ipv6_address_on_creation = true
+
+  public_subnet_ipv6_prefixes   = [0, 1, 2]
+  private_subnet_ipv6_prefixes  = [3, 4, 5]
+  database_subnet_ipv6_prefixes = [6, 7, 8]
 
   tags = local.tags
 }
